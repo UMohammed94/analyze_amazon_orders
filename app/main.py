d64f1acaae7a929a1cd1cc6c9317167a9ca05efa
Mohammed Uddin <uddin.mohammed94@gmail.com>
2022-08-09 18:10:37 -0400

    added

--- app/main.py
+++ app/main.py
@@ -9,10 +9,27 @@ df["Item Total"] = df["Item Total"].str.replace('$','').astype(float)
 first_five_rows=df.head()
 columns_and_rows=df.shape
 
-total_charged=df["Item Total"].sum()
+item_total_column=df["Item Total"]
+spend_total=item_total_column.sum()
 
-print('This is my df: ', '\n\n\n',first_five_rows)
-print('this is how much I spent on amazon: $', total_charged)
+print('this is how much I spent on amazon: $', spend_total)
+
+spend_average_per_order=item_total_column.mean()
+
+print('this is my avg. spend on amazon: $', spend_average_per_order)
+
+median_spend_per_order=item_total_column.median()
+
+print('this is my median spend on amazon: $', median_spend_per_order)
+
+max_spend_per_order=item_total_column.max()
+
+print('this is my max spend on amazon: $', max_spend_per_order)
+
+min_spend_per_order=item_total_column.min()
+
+print('this is my min spend on amazon: $', min_spend_per_order)
+
 
 
 
