84a21b46c79c1b733f0b9c76e5c760ac5d4e608e
Mohammed Uddin <uddin.mohammed94@gmail.com>
2022-08-10 17:05:58 -0400

    function conversion

--- app/config.py
+++ app/config.py
@@ -6,3 +6,6 @@ CSV='/csv_files/amazon-orders.csv'
 
 df=pd.read_csv(str(PARENT_DIR) + CSV)
 df=df.fillna(0)
+
+ORDER_TOTAL=df["Item Total"].str.replace('$','',regex=True).astype(float)
+ORDER_TAX_CHARGED=df["Item Subtotal Tax"].str.replace('$','',regex=True).astype(float)
--- app/main.py
+++ app/main.py
@@ -1,38 +1,57 @@
 import pandas as pd
-from pathlib import Path
-from config import df
+from config import df, ORDER_TOTAL, ORDER_TAX_CHARGED
 
-order_total=df["Item Total"].str.replace('$','',regex=True).astype(float)
+def yourSpendTotal(order):
+    spend_total=order.sum()
+    print('this is how much I spent on amazon: $', spend_total)
+    
+    return spend_total
 
-spend_total=order_total.sum()
+def yourAveragePerOrder(order):
+    spend_average_per_order=order.mean()
+    print('this is my avg. spend on amazon: $', spend_average_per_order)
 
-print('this is how much I spent on amazon: $', spend_total)
+    return spend_average_per_order
 
-spend_average_per_order=order_total.mean()
+def yourMedianPerOrder(order):
+    median_spend_per_order=order.median()
+    print('this is my median spend on amazon: $', median_spend_per_order)
 
-print('this is my avg. spend on amazon: $', spend_average_per_order)
+    return median_spend_per_order
 
-median_spend_per_order=order_total.median()
+def yourMaxPerOrder(order):
+    max_spend_per_order=order.max()
+    print('this is my median spend on amazon: $', max_spend_per_order)
 
-print('this is my median spend on amazon: $', median_spend_per_order)
+    return max_spend_per_order
 
-max_spend_per_order=order_total.max()
+def yourMinPerOrder(order):
+    min_spend_per_order=order.max()
+    print('this is my min spend on amazon: $', min_spend_per_order)
 
-print('this is my max spend on amazon: $', max_spend_per_order)
+    return min_spend_per_order
 
-min_spend_per_order=order_total.min()
+def yourTotalTaxCharged(tax_charged):
+    total_tax_charged=tax_charged.sum()
+    print('this is total tax charged on amazon: $', total_tax_charged)
 
-print('this is my min spend on amazon: $', min_spend_per_order)
+    return total_tax_charged
 
-order_tax_charged=df["Item Subtotal Tax"].str.replace('$','',regex=True).astype(float)
+def yourEffectiveSalesTaxRate():
+    spend_total=ORDER_TOTAL.sum()
+    total_tax_charged=ORDER_TAX_CHARGED.sum
+    effective_sales_tax_rate=((total_tax_charged/spend_total)*100)
+    print('this is my effective sales tax rate: %',effective_sales_tax_rate)
 
-total_tax_charged=order_tax_charged.sum()
+    return effective_sales_tax_rate
 
-print(total_tax_charged)
 
-effective_sales_tax_rate=((total_tax_charged/spend_total)*100)
 
-print('this is my effective sales tax rate: %',effective_sales_tax_rate)
+# todo what time of day do I buy more
+# todo how long do I keep something in my cart before I purchase
+# todo what day of the week am I more likely to purchase
+# todo essential purchase vs none essential purchase - look at your most popular category
+# 
 
 
 
