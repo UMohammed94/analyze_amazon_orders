b2c76106d8a2660cd40b5f26229b5576237266f6
Mohammed Uddin <uddin.mohammed94@gmail.com>
2022-08-10 01:28:49 -0400

    added more stuff

--- app/main.py
+++ app/main.py
@@ -1,36 +1,45 @@
 import pandas as pd
 from pathlib import Path
 
+
 PARENT_DIR = Path.cwd().parent
 
 df=pd.read_csv(str(PARENT_DIR) + '/csv_files/amazon-orders.csv')
 df=df.fillna(0)
-df["Item Total"] = df["Item Total"].str.replace('$','').astype(float)
-first_five_rows=df.head()
-columns_and_rows=df.shape
 
-item_total_column=df["Item Total"]
-spend_total=item_total_column.sum()
+order_total=df["Item Total"].str.replace('$','',regex=True).astype(float)
+
+spend_total=order_total.sum()
 
 print('this is how much I spent on amazon: $', spend_total)
 
-spend_average_per_order=item_total_column.mean()
+spend_average_per_order=order_total.mean()
 
 print('this is my avg. spend on amazon: $', spend_average_per_order)
 
-median_spend_per_order=item_total_column.median()
+median_spend_per_order=order_total.median()
 
 print('this is my median spend on amazon: $', median_spend_per_order)
 
-max_spend_per_order=item_total_column.max()
+max_spend_per_order=order_total.max()
 
 print('this is my max spend on amazon: $', max_spend_per_order)
 
-min_spend_per_order=item_total_column.min()
+min_spend_per_order=order_total.min()
 
 print('this is my min spend on amazon: $', min_spend_per_order)
 
+order_tax_charged=df["Item Subtotal Tax"].str.replace('$','',regex=True).astype(float)
 
+total_tax_charged=order_tax_charged.sum()
+
+print(total_tax_charged)
+
+effective_sales_tax_rate=((total_tax_charged/spend_total)*100)
+
+print('this is my effective sales tax rate: %',effective_sales_tax_rate)
+
+order_date = pd.to_datetime(df['Order Date'])
 
 
 
